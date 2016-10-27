e7225f777179e05b2c37529fcff6f567392ba98c
Gabriel Erdelyi <erdgabios@gmail.com>
2016-10-27 16:22:11 +0200

    Initial Commit

---
+++ openweather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openweather
+//
+//  Created by G on 27/10/2016.
+//  Copyright © 2016 erdgabios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
